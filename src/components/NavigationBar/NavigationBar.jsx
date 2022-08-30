b809b9cb0dda57fbd67e8277d6c24afe463c2324
Connor <connorturlan@gmail.com>
2022-08-31 09:04:50 +0930

    updated styles and added click to hide for navbar

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,18 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./NavigationBar.module.scss";
 
 function NavigationBar({ children }) {
-	const [hidden, setHidden] = useState(true);
+	const hide = () => {
+		document.getElementById("navbarToggle").checked = false;
+	};
 
 	return (
 		<nav className={styles.NavigationBar}>
-			<input type="checkbox" className={styles.NavigationBar__Toggle} />
+			<input
+				id="navbarToggle"
+				type="checkbox"
+				className={styles.NavigationBar__Toggle}
+			/>
 			<label
 				className={styles.NavigationBar__ToggleLabel}
 				htmlFor="NavigationBar__Menu"
 			></label>
 			<h1 className={styles.NavigationBar__Icon}>Connor Turlan</h1>
-			<div className={styles.NavigationBar__Buttons}>{children}</div>
+			<div onClick={hide} className={styles.NavigationBar__Buttons}>
+				{children}
+			</div>
 		</nav>
 	);
 }
