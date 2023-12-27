c1398d0613a4a4706542330b0279f8eebe5e0ce8
connor <connorturlan@gmail.com>
2023-12-27 10:37:22 +1030

    rm nology, add langs

--- src/components/BusinessCard/BusinessCard.jsx
+++ src/components/BusinessCard/BusinessCard.jsx
@@ -2,26 +2,24 @@ import Card from "../CardList/Card/Card";
 import styles from "./BusinessCard.module.scss";
 
 function BusinessCard(props) {
-	return (
-		<Card isEven={false} className={styles.BusinessCard}>
-			<h2 className={styles.BusinessCard__Title}>
-				Hi, my name is Connor!
-			</h2>
-			<h3 className={styles.BusinessCard__Subtitle}>This is me</h3>
-			<article className={styles.BusinessCard__Content}>
-				<ul className={styles.BusinessCard__List}>
-					<li>Computer Scientist</li>
-					<li>Programmer</li>
-					<li>_nologist</li>
-				</ul>
-				<img
-					src="images/connor.jpg"
-					alt="Connor"
-					className={styles.BusinessCard__Image}
-				/>
-			</article>
-		</Card>
-	);
+  return (
+    <Card isEven={false} className={styles.BusinessCard}>
+      <h2 className={styles.BusinessCard__Title}>Hi, my name is Connor!</h2>
+      <h3 className={styles.BusinessCard__Subtitle}>This is me</h3>
+      <article className={styles.BusinessCard__Content}>
+        <ul className={styles.BusinessCard__List}>
+          <li>Computer Scientist</li>
+          <li>Software Engineer</li>
+          <li>Programmer</li>
+        </ul>
+        <img
+          src="images/connor.jpg"
+          alt="Connor"
+          className={styles.BusinessCard__Image}
+        />
+      </article>
+    </Card>
+  );
 }
 
 export default BusinessCard;
--- src/components/LanguageCard/LanguageCard.jsx
+++ src/components/LanguageCard/LanguageCard.jsx
@@ -2,33 +2,37 @@ import Card from "../CardList/Card/Card";
 import styles from "./LanguageCard.module.scss";
 
 function LanguageCard(props) {
-	return (
-		<Card isEven={false} className={styles.LanguageCard}>
-			<h2 className={styles.LanguageCard__Title}>Languages and Tools</h2>
-			<div className={styles.LanguageCard__Grid}>
-				<i className="devicon-c-plain"></i>
-				<i className="devicon-cplusplus-plain"></i>
-				<i className="devicon-csharp-plain"></i>
-				<i className="devicon-python-plain-wordmark"></i>
-				<i className="devicon-pytest-plain-wordmark"></i>
-				<i className="devicon-html5-plain-wordmark"></i>
-				<i className="devicon-css3-plain-wordmark"></i>
-				<i className="devicon-sass-plain"></i>
-				<i className="devicon-javascript-plain"></i>
-				<i className="devicon-jest-plain" title="Jest"></i>
-				<i className="devicon-nestjs-plain-wordmark"></i>
-				<i className="devicon-react-original-wordmark"></i>
-				<i className="devicon-vuejs-plain-wordmark"></i>
-				<i className="devicon-mysql-plain-wordmark"></i>
-				<i className="devicon-sqlite-plain-wordmark"></i>
-				<i className="devicon-firebase-plain-wordmark"></i>
-				<i className="devicon-java-plain-wordmark"></i>
-				<i className="devicon-spring-plain-wordmark"></i>
-				<i className="devicon-bash-plain" title="Bash"></i>
-				<i className="devicon-github-original-wordmark"></i>
-			</div>
-		</Card>
-	);
+  return (
+    <Card isEven={false} className={styles.LanguageCard}>
+      <h2 className={styles.LanguageCard__Title}>Languages and Tools</h2>
+      <div className={styles.LanguageCard__Grid}>
+        <i className="devicon-c-plain"></i>
+        <i className="devicon-cplusplus-plain"></i>
+        <i className="devicon-csharp-plain"></i>
+        <i className="devicon-python-plain-wordmark"></i>
+        <i className="devicon-pytest-plain-wordmark"></i>
+        <i class="devicon-docker-plain-wordmark"></i>
+        <i class="devicon-kubernetes-plain-wordmark"></i>
+        <i class="devicon-go-original-wordmark"></i>
+        <i class="devicon-argocd-plain-wordmark"></i>
+        <i className="devicon-html5-plain-wordmark"></i>
+        <i className="devicon-css3-plain-wordmark"></i>
+        <i className="devicon-sass-plain"></i>
+        <i className="devicon-javascript-plain"></i>
+        <i className="devicon-jest-plain" title="Jest"></i>
+        <i className="devicon-nestjs-plain-wordmark"></i>
+        <i className="devicon-react-original-wordmark"></i>
+        <i className="devicon-vuejs-plain-wordmark"></i>
+        <i className="devicon-mysql-plain-wordmark"></i>
+        <i className="devicon-sqlite-plain-wordmark"></i>
+        <i className="devicon-firebase-plain-wordmark"></i>
+        <i className="devicon-java-plain-wordmark"></i>
+        <i className="devicon-spring-plain-wordmark"></i>
+        <i className="devicon-bash-plain" title="Bash"></i>
+        <i className="devicon-github-original-wordmark"></i>
+      </div>
+    </Card>
+  );
 }
 
 export default LanguageCard;
